ea5c785772ebdc227f48b8031b959dc5fc55d19e
Ruslan Baroev <ruslanbaroev@Air-Ruslan.IGD_Rostelecom>
2023-08-30 21:56:19 +0300

    add project

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <el-button></el-button>
+    </div>
+    <div class="header__links">
+        <NuxtLink to="/">Обо мне</NuxtLink>
+        <NuxtLink to="/">Портфолио</NuxtLink>
+    </div>
+  </header>
+</template>
+<script setup>
+</script>
+<style lang="scss">
+.header {
+
+    &__logo {
+    }
+    &__links {
+    }
+}
+</style>

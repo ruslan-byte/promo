4db8a95b4f796a4cca827c5a5334c2923875f436
ruslan-byte <rbaroev0@gmail.com>
2023-09-05 14:45:41 +0300

    Updates

---
+++ assets/index.48f128e2.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as s,j as a}from"./entry.261135fb.js";const r={},l={class:"welcome container"},i=s("h2",{class:"welcome__title"},"Я создаю сайты",-1),d=[i];function m(_,t){return c(),o("section",l,d)}const f=e(r,[["render",m]]),u={};function p(_,t){const n=f;return c(),a(n)}const h=e(u,[["render",p]]);export{h as default};
